154171e3b43dabe50ba79f36fd826ed8f1e4dda3
danielemariano <70856686+danielemariano@users.noreply.github.com>
2021-03-15 12:57:18 +0100

    Sesta commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,39 +1,14 @@
 package main;
 
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HelloWorld {
 
-	/*private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
-    public static void main(String[] args) throws SecurityException, IOException {
- 
-        LOGGER.info("Logger Name: "+LOGGER.getName());
-         
-        LOGGER.warning("Can cause ArrayIndexOutOfBoundsException");
-         
-        //An array of size 3
-        int []a = {1,2,3};
-        int index = 4;
-        LOGGER.config("index is set to "+index);
-         
-        try{
-            System.out.println(a[index]);
-        }catch(ArrayIndexOutOfBoundsException ex){
-            LOGGER.log(Level.SEVERE, "Exception occur", ex);
-        }
-         
-    }*/
 	
 	private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
 	
-	public static void main(String[] args) throws SecurityException, IOException {
-			 
-		try{
-			LOGGER.log(Level.SEVERE, "Exception not occur");
-	    }catch(ArrayIndexOutOfBoundsException ex){
-	        LOGGER.log(Level.SEVERE, "Exception occur");
-	    }
+	public static void main(String[] args) {
+		LOGGER.log(Level.SEVERE, "Hello World");
 	}
 }
